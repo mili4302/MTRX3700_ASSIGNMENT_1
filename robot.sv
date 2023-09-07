864ee85aa685b904a9d05e4f466005520b26d652
yusen <1319740193@qq.com>
2023-09-07 19:48:12 +1000

    Added Run state, execute_done flag

--- robot.sv
+++ robot.sv
@@ -10,12 +10,15 @@ module robot (
     output reg [6:0] HEX3 // 
 );
 	
-    typedef enum logic [3:0] {IDLE = 4'b0000, Programming = 4'b0001, Execute = 4'b0010, Reset = 4'b0100} state_type;
+    typedef enum logic [3:0] {IDLE = 4'b0000, Programming = 4'b0001, Execute = 4'b0010, Run = 4'b0100, Reset = 4'b1000} state_type;
     state_type state, next_state;
 	
 	reg [7:0] command_counter = 0; // can count up to 256
 	reg [7:0] reset_counter = 0; // Used for resetting the command_array
 	reg [1:0] command_array[255:0]; // Array to store commands
+	reg [31:0] delay_counter = 32'd0;  // 32-bit counter to count up to 10^8
+	reg delay_done = 0;  // Flag to indicate if the delay is done
+	reg execute_done = 0; // Flag to indicate if all comands are excuted
 
 
 	// Next state logic
@@ -27,14 +30,21 @@ module robot (
 			end
 
 			Programming:begin
-				if (KEY[3]) next_state = Programming;
-				else if (KEY[2]) next_state = Execute;
+				if (KEY[3]) next_state = Programming; //Key[3] record command 
+				else if (KEY[2]) next_state = Execute; //Key[2] stop recording commands and go to Execute
+				else if (command_counter == 255) next_state = Execute;
 				else next_state = Programming;
 			end
 				
 			Execute: begin
 				if (KEY[1]) next_state = Reset;
-				else next_state = Execute;
+				else if (execute_done) next_state = Reset; // When execute_done == 1, go to reset
+				else next_state = Run;
+			end
+
+			Run: begin
+				if (delay_done) next_state = Execute;  // Change to whatever state you want after the delay
+				else next_state = Run;  // Stay in the Run state while waiting
 			end
 
 			Reset: begin
@@ -92,19 +102,28 @@ module robot (
 								LEDR <= 18'b1111000011110000;
 							end
 
-							command_counter <= command_counter - 8'd1;
+						if (command_counter > 0) command_counter <= command_counter - 8'd1;
+						else execute_done <= 1;
+							
  
 						end
 
+					Run:
+						begin
+							if (delay_counter < 32'd100000000) begin
+								delay_counter = delay_counter + 32'd1; //跑一亿次，刚好两秒
+							end
+							else begin
+								delay_done = 1'b1;  // Set the delay done flag
+								delay_counter = 32'd0;  // Reset the counter
+						end
+						end
 
-
-                	
-
-				Reset:
-				begin
-					command_array[reset_counter] <= 0;
-					reset_counter = reset_counter + 1;	
-				end
+					Reset:
+					begin
+						command_array[reset_counter] <= 0;
+						reset_counter = reset_counter + 1;	
+					end
 			endcase
 		end
 
