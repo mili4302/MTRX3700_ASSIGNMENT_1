dfb08efb498c0ce4e7c8d7f685f5d849cd92d7cb
jliu4988 <126042609+jliu4988@users.noreply.github.com>
2023-09-09 18:26:51 +1000

    Update debounce.v

--- debounce.v
+++ debounce.v
@@ -2,27 +2,24 @@ module debounce (
     input clk, button,
     output reg button_pressed
 );
-  localparam delay_val = 3;
+  localparam delay_val = 2500/*FILL-IN*/; // 50us with clk period 20ns is ____ counts
 
   // Your code here!
-  reg [11:0] counts;
+  reg [11:0] counter = 0;
+  reg last_button_state = 1'b1; // Assuming the button is initially not pressed
 
-  always@(posedge clk)
-  begin
-    if(button)begin
-      if(counts == delay_val)begin
-        button_pressed <= 1;
-      end
-      else begin
-        button_pressed <= 0;
-      end
-      counts <= counts + 1;
+  always @(posedge clk) begin
+    if (button != last_button_state) begin // button is pressed
+      counter <= counter + 1; // debounce process
+    end else begin
+      counter <= 0;
     end
-    else begin
-      counts <= 0;
-     end
+
+    if (counter >= delay_val) begin
+      button_pressed <= button;
+      last_button_state <= button;
+    end
   end
-  
 endmodule
 
 
