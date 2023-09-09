3dab943164a1444ac0498da171449453af631c89
Dkun74 <sudikun7428@gmail.com>
2023-09-09 15:58:01 +1000

    update

--- motor_control.sv
+++ motor_control.sv
@@ -1,7 +1,7 @@
 module motor_control (
     input   clk,
     input   [1:0]   direction,
-    input   [1:0]   torque
+    input   [1:0]   torque,
     output  [8:0]   right_motor, left_motor
 );
     always_ff @(posedge clk) begin
@@ -32,7 +32,7 @@ module motor_control (
                                (torque == 2'b01) ? 00001000 :
                                (torque == 2'b10) ? 00001100 : 00001111;
 
-                left_motorr <= (torque == 2'b00) ? 00000000 :
+                left_motor <= (torque == 2'b00) ? 00000000 :
                                (torque == 2'b01) ? 00000000 :
                                (torque == 2'b10) ? 00001000 : 00001100;
             end
